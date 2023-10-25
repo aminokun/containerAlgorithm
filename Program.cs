5df5c0934afa87d1ec9d7af199cb0206a7796c42
amine <amine.dbh99@gmail.com>
2023-10-25 09:55:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace containership
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship ship = new Ship(2, 3);
+            Console.WriteLine(ship.ToString());
+
+        }
+    }
+}
